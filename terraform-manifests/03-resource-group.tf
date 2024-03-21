517789ce96d86a9420f0ea2e129ebc59f4f80e3a
luniemma <luniyisiemmanuel@gmail.com>
2024-03-21 14:34:19 -0400

    Adjust Variables

--- terraform-manifests/03-resource-group.tf
+++ terraform-manifests/03-resource-group.tf
@@ -1,4 +1,6 @@
 # Terraform Resource to Create Azure Resource Group with Input Variables defined in variables.tf
+# Updated 03-resource-group.tf
+
 resource "azurerm_resource_group" "aks_rg" {
   name     = "${var.resource_group_name}-${var.environment}"
   location = var.location
