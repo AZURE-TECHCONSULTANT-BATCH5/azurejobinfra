9ac53efad3d02bc184f90f03f0c09a1270616ef6
luniemma <luniyisiemmanuel@gmail.com>
2024-03-21 13:44:50 -0400

    removed some unexpected files

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -10,7 +10,6 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name                 = "systempool"
     vm_size              = "Standard_DS2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    availability_zones   = [1, 2, 3]
     enable_auto_scaling  = true
     max_count            = 3
     min_count            = 1
@@ -35,22 +34,10 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 
   # Azure Policy and OMS Agent moved to top level
-  azure_policy {
-    enabled = true
-  }
 
-  oms_agent {
-    enabled                    = true
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
-  }
+  
 
-  role_based_access_control {
-    enabled = true
-    azure_active_directory {
-      managed                = true
-      admin_group_object_ids = [azuread_group.aks_administrators.id]
-    }
-  }
+
 
   windows_profile {
     admin_username = var.windows_admin_username
